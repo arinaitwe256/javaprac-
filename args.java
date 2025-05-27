1788098c0e95ad40d39998c4e4a5616250a9a32f
arinaitwe256 <davanarinaitwe@gmail.com>
2025-05-27 09:00:21 +0300

    Add files via upload

---
+++ args.java
@@ -0,0 +1,7 @@
+class useArgument{
+     public static void main(String args[]){
+         System.out.println("hi,");
+         System.out.println(args[0]);
+         System.out.println("how are you");
+     }
+ }
